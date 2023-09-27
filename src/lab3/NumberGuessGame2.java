b477bc2ee25d8d307cbf12781cad90b749c026b6
salahmajid <salahmajid@icloud.com>
2023-09-26 22:20:49 -0500

    set lower and upper bound constant

--- src/lab3/NumberGuessGame2.java
+++ src/lab3/NumberGuessGame2.java
@@ -4,16 +4,16 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuessGame2 {
-	static final int BOUND = 101;
-
+	static final int UPPER_BOUND = 100;
+	static final int LOWER_BOUND = 0;
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
 		Random gen = new Random();
-		int randNum = gen.nextInt(BOUND);
+		int randNum = gen.nextInt(LOWER_BOUND, UPPER_BOUND + 1);
 		System.out.println(randNum);
 
-		System.out.println("Guess an integer between 0 and 100: ");
+		System.out.printf("Guess an integer between %d and %d: ", LOWER_BOUND, UPPER_BOUND);
 
 		int userGuessNum = scan.nextInt();
 
